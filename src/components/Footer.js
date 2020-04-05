0d4c75698a53c10c1828bc71e88c60a0aa9e6582
Joel Underwood <joelrunderwood@gmail.com>
2020-04-05 00:05:00 -0400

    fixed spelling error in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className="footer">
-            <p>This appl was developed by Joel Underwood with the tutorial by Kyle from Web Dev Simplified. Check out Kyle's website <a href="https://courses.webdevsimplified.com/">here</a>. He has really great content on <a href="https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw">YouTube</a> as well. Highly recommended.</p>
+            <p>This app was developed by Joel Underwood with a tutorial by Kyle from Web Dev Simplified. Check out Kyle's website <a href="https://courses.webdevsimplified.com/">here</a>. He has really great content on <a href="https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw">YouTube</a> as well. Highly recommended.</p>
         </div>
     )
 }
